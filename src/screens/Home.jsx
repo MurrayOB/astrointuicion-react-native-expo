82071c6cb9c657e3077dc20f74fcbe61441e33ce
Murray O'Brien <61970823+murrayobrienIT@users.noreply.github.com>
2021-08-09 12:35:34 +0200

    Initial Commit

---
+++ src/screens/Home.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from '../components/Themed';
+
+const Home = () => {
+    return (
+        <View>
+           <Text>This is the Home Screen</Text> 
+        </View>
+    ); 
+}
+
+export default Home; 
---
+++ src/screens/account/Login.tsx
@@ -0,0 +1,89 @@
+import * as React from "react";
+import { View, Text } from "../../components/Themed";
+import { StyleSheet, Button, TextInput, TouchableOpacity } from "react-native";
+import axios from "axios";
+
+const Login = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [response, setResponse] = React.useState("");
+
+  const handleLogin = () => {
+    if(email == '' || password == ''){
+        setResponse('Username or Password cannot be empty')
+        return
+    }
+    
+    const headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+
+    const data = {
+        email: email, 
+        password: password
+    }
+
+    axios.post('http://127.0.0.1:8000/api/user/login', data, {headers: headers})
+        .then((res) => {
+            if(res.data.Success)
+            setResponse('Successfully logged in')
+            
+            
+        })
+        .catch((err) => {
+            const error = err.toString(); 
+            setResponse(error)
+        }); 
+
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setEmail}
+        value={email}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setPassword}
+        value={password}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleLogin} >
+            <Text style={styles.title}>
+                Login
+            </Text>
+        </TouchableOpacity>
+        <Text>{response}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    title: {
+        fontSize: 25,
+        fontWeight: "600",
+    },
+    input: {
+        height: 40,
+        width: 300,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        borderRadius: 8, 
+    },
+    button: {
+        backgroundColor: "rgb(110, 200, 100)", 
+        borderRadius: 20, 
+        padding: 20, 
+    }
+});
+
+export default Login;
