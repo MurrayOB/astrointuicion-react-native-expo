8d859079eba9e81baf05494d967dd93077398564
Murray O'Brien <61970823+murrayobrienIT@users.noreply.github.com>
2021-08-09 16:35:20 +0200

    React App

--- src/screens/Diary.tsx
+++ src/screens/Diary.tsx
@@ -5,7 +5,7 @@ import { Text, View } from '../components/Themed';
 export default function Diary() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Diary</Text>
+      <Text style={styles.title}>Diary Page</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
     </View>
   );
